461d51db1dc693b4e49449d07892651f388a3237
FrozenWinters <tkolomatski@hotmail.com>
2017-09-16 18:52:51 -0400

    two buttons

--- app.js
+++ app.js
@@ -26,9 +26,34 @@
     db.ref().child('Counter').child('count').set(value + 1);
   }
 
-  function push_coordinates(x, y){
+  function pushmesh(x, y){
     db.ref().child('Position').child('x_val').set(x);
     db.ref().child('Position').child('y_val').set(y);
+    v = geometry.vertices;
+    var points = [];
+    for(i = 0; i < mesh_res * mesh_res; i++){
+      points[i] = v[i].y;
+      if(!(i % 500))
+        console.log(v[i].y);
+    }
+    //db.ref().child('Mesh').set(points);
+    db.ref().child('Mesh').set(v);
+  }
+
+  function subscribe(){
+    db.ref().child('Mesh').on("value", function(s){
+      window.wave_heights = s.val();
+      window.is_pull = true;
+      console.log(window.wave_heights);
+      /*v = geometry.vertices;
+      for(i = 0; i < mesh_res * mesh_res; i++){
+        v[i].y = (s.val())[i];
+      }
+      geometry.verticesNeedUpdate = true;
+      geometry.computeFaceNormals();
+      geometry.computeVertexNormals();
+      geometry.normalsNeedUpdate = true;*/
+    });
   }
 
   //Seting up the DOM
@@ -44,20 +69,25 @@
   }
 
   App.defaults = {
-    width: 500,
-    height: 500
+    width: 100,
+    height: 100
   };
 
   function writeDOM(app) {
     var html ='';
-    html += '<div class="screen" style="width:' + app.option.width + 'px; height:' + app.option.height +'px; background:blue;"></div>';
+    html += '<div class="send" style="width:' + app.option.width + 'px; height:'
+      + app.option.height +'px; background:blue; z-index: 10; position:relative; top: -210px; left: 20px;"></div>';
+    html += '<div class="receive" style="width:' + app.option.width + 'px; height:'
+      + app.option.height +'px; background:red; z-index: 10; position:relative; top: -310px; left: 150px;"></div>';
     app.wrapper.html(html);
-    app.wrapper.find('.screen').on("touchend mouseup", function (e) {
+    app.wrapper.find('.send').on("touchend mouseup", function (e) {
       var x_val = e.pageX - $(this).offset().left;
       var y_val = e.pageY - $(this).offset().top;
-      push_coordinates(x_val, y_val);
-			//pressHandler.call(this, e, board);
+      pushmesh(x_val, y_val);
 		});
+    app.wrapper.find('.receive').on("touchend mouseup", function (e) {
+      subscribe();
+    });
   }
 
   window.App = App;
@@ -67,6 +97,8 @@
 
 ;(function(){
   $(document).ready(function() {
+    window.is_pull = false;
+    add_graphics();
     var test = new App('#g_container', {});
     reset();
   });
