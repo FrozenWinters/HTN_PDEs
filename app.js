807c32f785d66534a077ff7ee1afdc54455a8aca
FrozenWinters <tkolomatski@hotmail.com>
2017-09-17 04:53:01 -0400

    Possible demoable version

--- app.js
+++ app.js
@@ -94,7 +94,8 @@
   $(document).ready(function() {
     window.is_pull = false;
     window.is_push = false;
-    add_graphics();
+    is_spect = false;
+    add_graphics(is_spect);
     var test = new App('#g_container', {});
   });
 }());
