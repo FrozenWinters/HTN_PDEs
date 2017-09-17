2091e0c58349e21370e10d8f21be1cf877a1ec23
FrozenWinters <tkolomatski@hotmail.com>
2017-09-17 06:57:46 -0400

    sdfds

--- wave/lib/DeviceOrientationControls.js
+++ wave/lib/DeviceOrientationControls.js
@@ -23,7 +23,7 @@
   function onDeviceOrientationChangeEvent(evt) {
     deviceOrientation = evt;
     console.log(evt);
-    deviceOrientation.gamma += Math.PI;
+    deviceOrientation.gamma += Math.PI * (3 / 4);
   }
   window.addEventListener('deviceorientation', onDeviceOrientationChangeEvent, false);
 
