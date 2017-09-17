0e768c327ec55bbdf508ad0e37e6f1e575f77146
FrozenWinters <tkolomatski@hotmail.com>
2017-09-17 07:16:50 -0400

    ASDF

--- wave/lib/DeviceOrientationControls.js
+++ wave/lib/DeviceOrientationControls.js
@@ -22,8 +22,9 @@
 
   function onDeviceOrientationChangeEvent(evt) {
     deviceOrientation = evt;
-    deviceOrientation.alpha += 45;
-    deviceOrientation.beta += 45;
+    deviceOrientation.alpha = 45;
+    deviceOrientation.gamma = 0;
+    deviceOrientation.beta = 45;
     console.log(evt);
   }
   window.addEventListener('deviceorientation', onDeviceOrientationChangeEvent, false);
